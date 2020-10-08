5293a340e9db23fd394f672be5b300ffafe24ccf
Victor Wang <wjp2013@gmail.com>
2020-10-08 23:35:16 +0800

    CHG: add example

--- example/main.go
+++ example/main.go
@@ -9,23 +9,23 @@ import (
 
 func main() {
 	device := &mtrta.RtaRequest_Device{
-		Os:              mtrta.RtaRequest_OperatingSystem.Enum(mtrta.RtaRequest_OS_ANDROID),
-		IdfaMd5Sum:      proto.String(""),
-		ImeiMd5Sum:      proto.String("725e5fa0ebf879a99da9eed8636ef7d2"),
-		AndroidIdMd5Sum: proto.String(""),
-		MacMd5Sum:       proto.String(""),
-		OaidMd5Sum:      proto.String(""),
-		Ip:              proto.String("106.121.177.97"),
-		Oaid:            proto.String("cfdfdcca-ffb7-cd28-f6df-777ff1cfc250"),
-		Ipv6:            proto.String(""),
+		Os: mtrta.RtaRequest_OperatingSystem.Enum(mtrta.RtaRequest_OS_ANDROID),
+		// IdfaMd5Sum:      proto.String(""),
+		ImeiMd5Sum: proto.String("0a50d917250da0101444e165b0d83bae"),
+		// AndroidIdMd5Sum: proto.String(""),
+		// MacMd5Sum:       proto.String(""),
+		// OaidMd5Sum:      proto.String(""),
+		Ip: proto.String("106.121.177.97"),
+		// Oaid:            proto.String(""),
+		// Ipv6:            proto.String(""),
 	}
 
 	rtaRequest := &mtrta.RtaRequest{
-		Id:     proto.String("187123abcde66661601193447"),
+		Id:     proto.String("99ace15790f118cee840a07967b04d24"),
 		IsPing: proto.Bool(true),
 		IsTest: proto.Bool(true),
 		Device: device,
-		SiteId: proto.String("20009"),
+		SiteId: proto.String("xxx"),
 	}
 
 	rtaurl := "https://gdtrtbdsp.meituan.com/rta?rta_site_param=netunion_rta"
@@ -35,5 +35,5 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("RequestId %v, Code %v, ProcessingTimeMs %d \n", rtaResponse.GetRequestId(), rtaResponse.GetCode(), rtaResponse.GetProcessingTimeMs())
+	fmt.Printf("RequestId %v, Code %v, GetPromotionTargetId %d \n", rtaResponse.GetRequestId(), rtaResponse.GetCode(), rtaResponse.GetPromotionTargetId())
 }
